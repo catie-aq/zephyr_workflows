ef9fa962427467cd84eb644cbd4d77b2cc7ec83d
Sébastien Delpeuch <s.delpeuch@catie.fr>
2024-03-14 10:35:20 +0100

    Add .gitignore and update_manifest files

---
+++ update_manifest/update_manifest/update.py
@@ -0,0 +1,15 @@
+class HelloWorld:
+    def __init__(self):
+        pass
+
+    def say_hello(self):
+        print("Hello, World!")
+
+
+def main():
+    hello = HelloWorld()
+    hello.say_hello()
+
+
+if __name__ == "__main__":
+    main()
